7daaccbc12189b09f3bc83353435665efcc1d551
Danilo Cava <danilo.cavallini.01@gmail.com>
2022-11-23 17:35:03 +0100

    ES2

--- TW_Esercizio2_02072018/src/Servlet/Integral.java
+++ TW_Esercizio2_02072018/src/Servlet/Integral.java
@@ -1,11 +1,6 @@
 package Servlet;
 
-import java.io.File;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Date;
 
 import javax.servlet.ServletConfig;
@@ -17,9 +12,6 @@ import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 
-import Beans.BeanCounter;
-import Beans.Result;
-
 public class Integral extends HttpServlet
 {
     private Gson gson;
@@ -29,12 +21,36 @@ public class Integral extends HttpServlet
         gson = new Gson();
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        session.setAttribute("startDate",new Date());
+        session.setAttribute("operateCount",0);
+        session.setAttribute("valid", true);
+        session.setMaxInactiveInterval(600);
+        this.getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
         float start = Float.parseFloat(request.getParameter("start"));
         float end  = Float.parseFloat(request.getParameter("end"));
-    
+        
         float result = ((end*end)/2f) - ((start*start)/2f);
 
+        synchronized(this) {
+            HttpSession session = request.getSession();
+        
+            Date sessionStart = (Date)session.getAttribute("startDate");
+            int operateCount = (int)session.getAttribute("operateCount");
+            session.setAttribute("operateCount", operateCount + 1);
+            System.out.println(operateCount);
+            if ( operateCount + 1 >= 20) {
+                session.setAttribute("valid", false);
+                this.getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+            }
+        
+        }
+        
         response.getWriter().println(gson.toJson(result));
     }
 }
