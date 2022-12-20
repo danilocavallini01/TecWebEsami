97d17865aaf3679b38e06872e2d7eb7bd62133bb
Danilo Cava <danilo.cavallini.01@gmail.com>
2022-12-20 11:12:06 +0100

    Update WSchat.java

--- TW_Esercizio3_26012022_B/src/Servlet/WSchat.java
+++ TW_Esercizio3_26012022_B/src/Servlet/WSchat.java
@@ -2,6 +2,8 @@ package Servlet;
 
 import java.io.IOException;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -104,6 +106,17 @@ public class WSchat {
 			e.printStackTrace();
 		}
 	}
+
+	@OnClose
+	public void onClose(Session session) {
+		//
+	}
+
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		//
+	}
+
 
 	private String checkWin() {
 		
